00a7d5371af13be547cf5c452caaa3b9fa5a1052
AdBean <AdbeanX@outlook.com>
2022-06-09 09:52:15 +0800

    fix: make svg outer box overflow (#103)

--- src/theme/Drawer/index.js
+++ src/theme/Drawer/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import "./style.css";
 
 export default function Drawer(props) {
+  const { SVG } = props;
+
   return (
-    <div className="svg-box">
+    <div className="svg-box" style={{}}>
       <svg
         className="svg-container"
-        style={{ marginTop: "20px" }}
+        style={{ marginTop: "20px", width: SVG.attrs.width }}
         ref={(element) => {
-          element && element.appendChild(props.SVG.toSVG());
+          element && element.appendChild(SVG.toSVG());
         }}
       ></svg>
     </div>
