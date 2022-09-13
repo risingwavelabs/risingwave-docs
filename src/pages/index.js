d25855dd6f219c35ab383fa4e08fa8c1c95006e7
Mike Wang <52522981+mikechesterwang@users.noreply.github.com>
2022-09-13 12:32:02 +0800

    hide landing page (#278)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from "react";
 export default function Home() {
   useEffect(() => {
-    // window.location.href = window.location.href + "docs/latest/intro";
-    window.location.href = window.location.href + "landing";
+    window.location.href = window.location.href + "docs/latest/intro";
+    // window.location.href = window.location.href + "landing";
   }, []);
 
   return (<>
