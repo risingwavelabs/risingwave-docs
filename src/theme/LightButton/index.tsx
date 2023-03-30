c248d18e8e482c73e38ecc2db92c0ec9ea20fb72
AdBean <AdbeanX@outlook.com>
2023-03-30 20:35:37 +0800

    feat: attribute for button to redirect to cloud docs (#703)

--- src/theme/LightButton/index.tsx
+++ src/theme/LightButton/index.tsx
@@ -7,10 +7,11 @@ type Props = {
   text: string;
   doc?: string;
   url?: string;
+  cloud?: string;
   block?: boolean;
 };
 
-export default function LightButton({ text, doc, url, block }: Props) {
+export default function LightButton({ text, doc, url, block, cloud }: Props) {
   const history = useHistory();
   const { globalData } = useDocusaurusContext();
   const location = useLocation();
@@ -19,13 +20,17 @@ export default function LightButton({ text, doc, url, block }: Props) {
     <button
       onClick={() => {
         if (doc) {
-          globalData["docusaurus-plugin-content-docs"].default["versions"].map((v) => {
-            if (location.pathname.includes(v.path)) {
-              history.push(`${v.path}/${doc}`);
+          globalData["docusaurus-plugin-content-docs"].default["versions"].map(
+            (v) => {
+              if (location.pathname.includes(v.path)) {
+                history.push(`${v.path}/${doc}`);
+              }
             }
-          });
+          );
         } else if (url) {
           window.open(url, "_blank", "noopener,noreferrer");
+        } else if (cloud) {
+          history.push(`/cloud/${cloud}`);
         }
       }}
       className={block ? "light block" : "light"}
--- src/theme/RollButton/index.tsx
+++ src/theme/RollButton/index.tsx
@@ -6,10 +6,11 @@ import "./style.css";
 type Props = {
   text: string;
   doc?: string;
+  cloud?: string;
   url?: string;
 };
 
-export default function RollButton({ text, doc, url }: Props) {
+export default function RollButton({ text, doc, url, cloud }: Props) {
   const history = useHistory();
   const { globalData } = useDocusaurusContext();
   const location = useLocation();
@@ -19,13 +20,17 @@ export default function RollButton({ text, doc, url }: Props) {
       <button
         onClick={() => {
           if (doc) {
-            globalData["docusaurus-plugin-content-docs"].default["versions"].map((v) => {
+            globalData["docusaurus-plugin-content-docs"].default[
+              "versions"
+            ].map((v) => {
               if (location.pathname.includes(v.path)) {
                 history.push(`${v.path}/${doc}`);
               }
             });
           } else if (url) {
             window.open(url, "_blank", "noopener,noreferrer");
+          } else if (cloud) {
+            history.push(`/cloud/${cloud}`);
           }
         }}
         className="button-3 learn-more"
