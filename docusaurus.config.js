1e45c50f89081149af0262c170c35f8041979a3b
AdBean <AdbeanX@outlook.com>
2023-07-31 16:15:24 +0800

    feat(build): use swc-loader to speed up the build by 20% (#1108)
    * feat: use swc-loader to speed up build
    
    * chore: remove d3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -235,6 +235,23 @@ const config = {
       async: true,
     },
   ],
+  webpack: {
+    jsLoader: (isServer) => ({
+      loader: require.resolve("swc-loader"),
+      options: {
+        jsc: {
+          parser: {
+            syntax: "typescript",
+            tsx: true,
+          },
+          target: "es2017",
+        },
+        module: {
+          type: isServer ? "commonjs" : "es6",
+        },
+      },
+    }),
+  },
 };
 
 async function createConfig() {
