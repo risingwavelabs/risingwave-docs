5618388cfe27b49eeb328048e771db0c92db3841
hengm3467 <100685635+hengm3467@users.noreply.github.com>
2024-04-25 13:56:51 +0800

    Update docusaurus.config.js (#2100)
    update shortcut from cmd+K to cmd+L.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -230,7 +230,7 @@ const config = {
       "runllm-server-address": "https://api.runllm.com",
       "runllm-assistant-id": "29",
       "runllm-position": "TOP_RIGHT",
-      "runllm-keyboard-shortcut": "Mod+k",
+      "runllm-keyboard-shortcut": "Mod+l",
       "runllm-theme-color": "#005EEC",
       "runllm-slack-community-url": "https://risingwave-community.slack.com/join/shared_invite/zt-2abrj3cbo-xnT_xn3_jd9piiM3vNPVdw",
       "runllm-name": "RisingWave",
